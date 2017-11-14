309ca4f6b4652a2750652fe2adcf642d04a5e0a1
Oliver <oliverdjurhuus@gmail.com>
2017-11-14 16:51:14 +0100

    done

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -2,13 +2,20 @@
 
 items = [26,18,45,38,59,49,44,12,11,1,55,50,3,7,35,41,14,43,39,57]
 
-
-def compare(e_1, e_2):
-	return (e_1, e_2) if e_1 < e_2 else (e_2,e_1)
+length_items = len(items)
 
 
-for element in items:
-	print element
+for i in xrange(length_items-1,0,-1):
+	swapped = False
+	for j in range(i):
+		if items[j+1] < items[j]:
+			items[j+1], items[j] = items[j], items[j+1]
+			swapped = True
+	if not swapped:
+		break;
+
+print items
+	
 
 
 
