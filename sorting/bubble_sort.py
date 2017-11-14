82e796a5c98c3909e8f51154ea4cfbf14ffa7ce1
Oliver <oliverdjurhuus@gmail.com>
2017-11-14 01:51:16 +0100

    test

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,25 @@
+
+
+items = [26,18,45,38,59,49,44,12,11,1,55,50,3,7,35,41,14,43,39,57]
+
+
+def compare(e_1, e_2):
+	return (e_1, e_2) if e_1 < e_2 else (e_2,e_1)
+
+
+for element in items:
+	print element
+
+
+
+
+
+
+
+
+
+
+
+
+
+
