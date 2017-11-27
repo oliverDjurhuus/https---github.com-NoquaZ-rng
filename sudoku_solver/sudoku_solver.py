611de47745558f0d82e6537373743f40862522f5
Oliver <oliverdjurhuus@gmail.com>
2017-11-27 18:30:56 +0100

    test sample update

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,43 +1,43 @@
 
-matrix2 = [
+matrix = [
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 0, 0, 0, 0, 1, 8, 0, 0
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+7, 0, 0, 0, 0, 0, 9, 0, 0
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 0, 0, 0, 0, 0, 0, 6, 5
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 0, 0, 0, 0, 2, 0, 0, 6
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 9, 8, 0, 0, 0, 1, 4, 0
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 3, 5, 0, 8, 0, 0, 0, 0
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+5, 0, 0, 0, 0, 0, 0, 3, 0
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 0, 0, 9, 1, 0, 7, 0, 2
 ],
 [
-0, 0, 0, 0, 0, 0, 0, 0, 0
+0, 1, 0, 6, 7, 0, 0, 0, 0
 ]
 ]
 
-matrix = [
+matrix_easy = [
 [
 6, 0, 0, 4, 3, 0, 0, 8, 0
 ],
 [
-1, 0, 5, 0, 0, 0, 0, 4, 3
+1, 0, 0, 5, 0, 0, 0, 4, 3
 ],
 [
-4, 0, 3, 0, 0, 8, 0, 5, 0
+4, 0, 3, 0, 8, 0, 0, 5, 0
 ],
 [
 8, 0, 1, 7, 0, 0, 0, 6, 0
@@ -215,8 +215,8 @@ def line_add_new_entries(matrix):
 pretty_print_matrix(matrix)
 pretty_print_matrix(make_candidates(matrix))
 
-for i in range(5):
-	#window_duplicate_exclusion_scan(matrix)
+for i in range(10):
+	window_duplicate_exclusion_scan(matrix)
 	line_exclusion(matrix)
 
 	line_add_new_entries(matrix)
